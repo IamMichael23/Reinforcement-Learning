b3699b646ff45db92dbbbb46a55af8941ff536d7
IamMichael23 <mlxcsdtc8180@gmail.com>
2026-03-08 19:58:38 -0400

    setting up

---
+++ src/gameSetUp.py
@@ -0,0 +1,17 @@
+from nes_py.wrappers import JoypadSpace
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import RIGHT_ONLY
+
+
+env = gym_super_mario_bros.make('SuperMarioBros-v0', apply_api_compatibility=True, render_mode='human')
+env = JoypadSpace(env, RIGHT_ONLY)
+
+done = True
+truncated = False
+for step in range(5000):
+    if done or truncated:
+        state = env.reset()
+    state, reward, done, truncated, info = env.step(env.action_space.sample())
+    env.render()
+
+env.close()
---
+++ src/wrapper.py
@@ -0,0 +1,28 @@
+import numpy as np
+from gym import Wrapper
+from gym.wrappers.frame_stack import FrameStack
+from gym.wrappers.gray_scale_observation import GrayScaleObservation
+from gym.wrappers.resize_observation import ResizeObservation
+
+
+class SkipFrame(Wrapper):
+    def __init__(self, env, skip):
+        super(SkipFrame, self).__init__(env)
+        self._skip = skip
+
+    def step(self, action):
+        total_reward = 0.0
+        done = False
+        for i in range(self._skip):
+            obs, reward, done, truncated, info = self.env.step(action)
+            total_reward += reward
+            if done or truncated:
+                break
+        return obs, total_reward, done, info
+    
+    def apply_wrapper(self, env):
+        env = SkipFrame(env, skip=4)
+        env = GrayScaleObservation(env)
+        env = ResizeObservation(env, shape=84)
+        env = FrameStack(env, num_stack=4, lz4_compress=True)
+        return env
